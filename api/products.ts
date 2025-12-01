e9b4feeeef23f9c23c933f3ac15c7314ef00d18a
omnisint <fabio.barosi@nedapretail.it>
2025-12-01 17:28:54 +0100

    update db.ts position

--- api/products.ts
+++ api/products.ts
@@ -1,6 +1,6 @@
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import pool from '../src/lib/db';
+import pool from '../lib/db';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
